020fc2d24d751950270715c0daf404fed8bb26ab
Ye HtetAung <yehtetaung7655@gmail.com>
2026-07-26 21:16:51 +0630

    Add sendMessage function for Telegram API

--- src/telegram.js
+++ src/telegram.js
@@ -1,1 +1,17 @@
-export {};
+export async function sendMessage(token, chatId, text) {
+  const url = `https://api.telegram.org/bot${token}/sendMessage`;
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: text,
+      parse_mode: "HTML"
+    })
+  });
+
+  return await response.json();
+}
