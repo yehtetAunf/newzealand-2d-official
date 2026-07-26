ac90778ba765079691f987921cc6bc57506980aa
Ye HtetAung <yehtetaung7655@gmail.com>
2026-07-26 21:11:08 +0630

    Add getMyanmarTime and formatResult functions
    Added functions to get Myanmar time and format results.

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,26 @@
+export function getMyanmarTime() {
+  return new Date().toLocaleString("en-US", {
+    timeZone: "Asia/Yangon"
+  });
+}
 
+export function formatResult(date, time, result) {
+  return `
+🇳🇿 New Zealand 2D Result
+
+🗓 ${date}
+🕐 ${time}
+
+🎯 Result - ${result}
+
+ကော်စား စုံစမ်းရန် 👇🏻👇🏻
+@NewZealand2D2026
+
+NewZealand 2D Live_Botကြည့်ရန်
+👉🏻👉🏻 @newzealand2dofficial_bot
+
+✅ Good Luck Everyone
+
+@JoinChannel
+`;
+}
