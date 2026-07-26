1def4fdb1d688493ef881502bc2456f65cb7e4f4
Ye HtetAung <yehtetaung7655@gmail.com>
2026-07-26 21:19:25 +0630

    Add fetchResult function to handle API requests

--- src/api.js
+++ src/api.js
@@ -1,1 +1,9 @@
-export {};
+export async function fetchResult(apiUrl) {
+  const response = await fetch(apiUrl);
+
+  if (!response.ok) {
+    throw new Error("API request failed");
+  }
+
+  return await response.json();
+}
