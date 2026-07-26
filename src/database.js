bab3ce7d284cf118757476bc16c14e5c1aa93913
Ye HtetAung <yehtetaung7655@gmail.com>
2026-07-26 21:18:14 +0630

    Add saveResult and getResult functions

--- src/database.js
+++ src/database.js
@@ -1,1 +1,7 @@
-export {};
+export async function saveResult(env, key, value) {
+  await env.DB.put(key, value);
+}
+
+export async function getResult(env, key) {
+  return await env.DB.get(key);
+}
