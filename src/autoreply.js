68633b3eba149173dffbda3365ffdf777e5d9588
Ye HtetAung <yehtetaung7655@gmail.com>
2026-07-27 08:48:46 +0630

    Update autoreply.js

--- src/autoreply.js
+++ src/autoreply.js
@@ -1,1 +1,7 @@
 import { CONFIG } from "./config.js";
+export async function handleMessage(text) {
+  const msg = text.trim().toLowerCase();
+  if (msg === "/start") {
+  return `🤖 ${CONFIG.BOT_NAME}`;
+      }
+}
