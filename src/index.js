76cca9c70169700c889f0af1f602354f59e6f07d
Ye HtetAung <yehtetaung7655@gmail.com>
2026-07-28 08:43:18 +0630

    Add addChannel and getChannels imports from database.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { fetchResult } from "./api.js";
 import { sendMessage, sendPhoto } from "./telegram.js";
-import { saveResult, getResult } from "./database.js";
+import { saveResult, getResult, addChannel, getChannels } from "./database.js";
 import { getMyanmarTime, formatResult } from "./utils.js";
 
 export default {
