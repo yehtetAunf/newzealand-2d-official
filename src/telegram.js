6ad737dda6249b41b884a020c691fc724139427f
Ye HtetAung <yehtetaung7655@gmail.com>
2026-07-28 08:14:26 +0630

    Add sendPhoto function to send images via Telegram

--- src/telegram.js
+++ src/telegram.js
@@ -15,3 +15,23 @@ export async function sendMessage(token, chatId, text) {
 
   return await response.json();
 }
+
+
+export async function sendPhoto(token, chatId, photo, caption = "") {
+  const url = `https://api.telegram.org/bot${token}/sendPhoto`;
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      photo: photo,
+      caption: caption,
+      parse_mode: "HTML"
+    })
+  });
+
+  return await response.json();
+}
