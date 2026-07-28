5ed99562b6a91ba281bab717574d19ea6dbb3519
Ye HtetAung <yehtetaung7655@gmail.com>
2026-07-28 08:28:12 +0630

    Add functions to insert and retrieve channels

--- src/database.js
+++ src/database.js
@@ -43,3 +43,39 @@ export async function getResult(env, key) {
 
   return result?.result || null;
 }
+
+export async function addChannel(env, channelId, channelName = "") {
+  await env.DB.prepare(
+    `
+    INSERT INTO channels (
+      channel_id,
+      channel_name
+    )
+    VALUES (?, ?)
+    ON CONFLICT(channel_id)
+    DO UPDATE SET
+      channel_name = excluded.channel_name,
+      is_active = 1,
+      updated_at = CURRENT_TIMESTAMP
+    `
+  )
+  .bind(
+    channelId,
+    channelName
+  )
+  .run();
+}
+
+
+export async function getChannels(env) {
+  const result = await env.DB.prepare(
+    `
+    SELECT channel_id
+    FROM channels
+    WHERE is_active = 1
+    `
+  )
+  .all();
+
+  return result.results || [];
+    }
